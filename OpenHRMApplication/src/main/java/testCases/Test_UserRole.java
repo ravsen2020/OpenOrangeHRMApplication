454d7d6fbf7fee9cae0b886e53d9008a0fb500c3
ravsen2020 <senthilkumar.venu@gmail.com>
2023-03-17 14:16:22 -0500

    Final commit 01

--- OpenHRMApplication/src/main/java/testCases/Test_UserRole.java
+++ OpenHRMApplication/src/main/java/testCases/Test_UserRole.java
@@ -18,7 +18,7 @@ public class Test_UserRole extends CommonFunctions{
 		logger.info("Navigating to the user page");
 		UserRole_Page_Objects.adminLink.click();
 		
-		//UserRole_Page_Objects.userManagementLink.click(); ***++..not needed 123 .***
+		//UserRole_Page_Objects.userManagementLink.click(); ***++..not needed 123 .5789***
 		logger.info("selecting the userRole");
 		UserRole_Page_Objects.userRole.click();
 		UserRole_Page_Objects.userRole.sendKeys("ESS");
