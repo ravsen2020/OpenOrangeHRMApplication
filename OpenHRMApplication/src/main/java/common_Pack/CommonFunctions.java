8ec49b5e9fe0f3d81336d0a04d6336f9183c9467
ravsen2020 <senthilkumar.venu@gmail.com>
2023-07-06 13:48:10 -0500

    today commit 6-7-2023

--- OpenHRMApplication/src/main/java/common_Pack/CommonFunctions.java
+++ OpenHRMApplication/src/main/java/common_Pack/CommonFunctions.java
@@ -5,20 +5,30 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CommonFunctions {
 	public static WebDriver driver=null;
 	public static Properties properties=null;
-	Logger logger=Logger.getLogger(CommonFunctions.class);  //factory design pattern info,debug,warn,fatel, error
+	
+	public static ExtentReports extent=null;
+	public static ExtentSparkReporter spark=null;
+	public static ExtentTest testCase=null;
+	
+	//Logger logger=Logger.getLogger(CommonFunctions.class);  //...factory design pattern info,debug,warn,fatel, error
+	
 	public Properties loadPropertiesFile() throws IOException
 	{	
 		
@@ -31,42 +41,55 @@ public class CommonFunctions {
 	@BeforeSuite
 	public void launchBrowser() throws IOException
 	{	
-		PropertyConfigurator.configure("log4j.properties");
-		logger.info("loding the property file");
+		//PropertyConfigurator.configure("log4j.properties");
+		//logger.info("loding the property file");
 		loadPropertiesFile();
 		
+		extent=new ExtentReports();
+		spark=new ExtentSparkReporter("D:\\OrageHRMReportFile.html");
+		extent.attachReporter(spark);
+		//*************************
+		extent.setSystemInfo("OS", "Windows 10");
+		extent.setSystemInfo("User Name", "Senthil");
+
+		spark.config().setDocumentTitle("Orange HRM Automation");
+		spark.config().setReportName("Ornage HRM Test Report");
+		spark.config().setTheme(Theme.STANDARD);
+		//**************************
 		String url=properties.getProperty("url");
 		String browser=properties.getProperty("browser");
 		
 		if(browser.equalsIgnoreCase("chrome"))
 		{
 			WebDriverManager.chromedriver().setup();
-			logger.info("launching chrome browse");
 			driver=new ChromeDriver();
 			
 		}
 		else if(browser.equalsIgnoreCase("fireFox"))
 		{
 			WebDriverManager.firefoxdriver().setup();
-			logger.info("launching firefox browse");
 			driver=new FirefoxDriver();
 			
 		}
 		driver.manage().window().maximize();
-		logger.info("navigating to the application");
 		driver.get(url);
 		
-		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);		
 		
 	} 
 	
+//	@Test
+//	public void tc_01testMethod()
+//	{
+//		System.out.println("This is for test purpose");
+//	}
 	
 	@AfterSuite
 	public void tearDown()
 	{
-		logger.info("After the execution, closing the browse");
-	//driver.quit();	
+		
+		//driver.quit();
+		extent.flush();
 	}
 
 }
